5dfcb4f69540d51b2ce4284b8b7d69fe96a6470d
Tim Saylor <tim.saylor@gmail.com>
2012-09-22 02:50:26 -0500

    import the signal method to the admin action

--- members/admin.py
+++ members/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from paypal.standard.ipn.models import PayPalIPN
 from paypal.standard.ipn.admin import PayPalIPNAdmin
-from .models import UserProfile
+from .models import UserProfile, paypal_to_profile
 
 def save_payment_to_profile(modeladmin, request, queryset):
     for obj in queryset.order_by('payment_date'):
